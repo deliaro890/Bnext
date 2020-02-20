f9ad58e2c92bfbed6aaf32f1a6370f683bef80ad
cris <delir890@gmail.com>
2020-02-20 14:23:54 -0600

    ya funciona el envio

--- funciones.js
+++ funciones.js
@@ -117,6 +117,7 @@ function requestData(){
         document.getElementById('surname_received').value=datos.data.surname;
         document.getElementById('email_received').value=datos.data.email;
         document.getElementById('phone_received').value=datos.data.phone;
+        document.getElementById('age_received').value=datos.data.age;
         
 
     });
@@ -124,7 +125,7 @@ function requestData(){
 
 
 
-}sendData
+}
 
 function sendData(){
 
@@ -132,22 +133,44 @@ function sendData(){
 	var api_url = 'https://api.bnext.io/partner_test/user/'
   var dir = api_url+id
 
-	$.getJSON(dir, function(data){
-		    var datos = data
-        console.log(data);
-        console.log(JSON.stringify(data, null, "  "));
+        var name = document.getElementById('name_received').value
+        var surname = document.getElementById('surname_received').value
+        var email = document.getElementById('email_received').value
+        var phone = document.getElementById('phone_received').value
+  			var amount = document.getElementById('loan_amount').value
+        var date = document.getElementById('loan_date').value
+        var weeks = document.getElementById('loan_weeks').value
+        var age = document.getElementById('age_received').value
 
-        document.getElementById('data').style.display='block'
-        document.getElementById('name_received').value=datos.data.name;
-        document.getElementById('surname_received').value=datos.data.surname;
-        document.getElementById('email_received').value=datos.data.email;
-        document.getElementById('phone_received').value=datos.data.phone;
-        
 
+
+	fetch(dir,{
+        method: 'POST',
+        headers: {
+            'X-WEB-KEY': 'Development'
+        },
+
+        //body: JSON.stringify({"id": 1, "name":'Cris', "surname": 'Huertas', 'email':'cris@next.es','phone':"553246888875",'age':35,'loan_amount':10,'loan_weeks':2,'loan_date':2}),
+        body: JSON.stringify({"id": id, "name":name, "surname": surname, 'email':email,'phone':phone,'age':age,'loan_amount':amount,'loan_weeks':weeks,'loan_date':date}),
+        cache: 'no-cache'
+    })
+    .then(function(response) {
+        return response.json();
+
+    })
+    .then(function(data) {
+        console.log('data = ', data);
+        console.log(data['code'])
+        if(data['code']==201){
+    	alert('Gracias en breve nos pondremos en contacto');
+    }
+
+    })
+    .catch(function(err) {
+        console.error(err);
     });
-
-
-
-
+    
+    
+    
 }
 
